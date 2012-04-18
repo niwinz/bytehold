8e62cc14a54aab8df20ac120c681351bb9ac8f90
Andrei Antoukh <niwi@niwi.be>
2012-04-18 23:07:35 +0200

    Add setup.py and MANIFEST file.

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from bytehold import __version__ as version
+
+setup(
+    name = "bytehold",
+    version = "{v[0]}.{v[1]}".format(v=version),
+    description = "Simple backup tool, which helps not to repeat scripts.",
+    author = "Andrei Antoukh",
+    author_emaul = "niwi@niwi.be",
+    url = "https://github.com/niwibe/bytehold",
+    py_modules = ["bytehold"],
+    scripts = ['bh'],
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Topic :: System :: Archiving :: Backup',
+        'Topic :: System :: Systems Administration',
+    ],
+)
